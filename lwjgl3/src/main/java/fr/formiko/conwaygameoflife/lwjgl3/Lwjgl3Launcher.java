a240c299a5eb2f5f72b800d8faa1dfc848594bf3
SEVI DERVISHI <sevidervishi@outlook.fr>
2023-11-17 23:18:10 +0100

    added -version arg

--- lwjgl3/src/main/java/fr/formiko/conwaygameoflife/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/fr/formiko/conwaygameoflife/lwjgl3/Lwjgl3Launcher.java
@@ -4,9 +4,27 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import fr.formiko.conwaygameoflife.ConwayGameOfLife;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
+        if (args.length > 0 && args[0].replace("-", "").equalsIgnoreCase("version")) {
+            try {
+                InputStream is = Lwjgl3Launcher.class.getClassLoader().getResourceAsStream("version.md");
+                String version = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8)).lines()
+                    .collect(Collectors.joining("\n"));
+                System.out.println(version);
+                System.exit(0);
+            } catch (Exception e) {
+                System.out.println("Fail to get version in DesktopLauncher.");
+            }
+        }
+
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
         createApplication();
     }
